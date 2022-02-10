0c84ead85df89b962892854c90eb76c1310b85fb
rocklee <xnettunnel@gmail.com>
2022-02-10 14:46:44 +0800

    .

---
+++ src/test/java/com/freestyle/netty/base/Test1.java
@@ -0,0 +1,12 @@
+package com.freestyle.netty.base;
+
+import org.junit.Test;
+
+/**
+ * Created by rocklee on 2022/2/8 14:45
+ */
+public class Test1 {
+  @Test
+  public void testByteBuf(){
+  }
+}
